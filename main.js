67eb4d0e22eb5a2971a34bb9844da5f7cc22513b
melek-houidi <melekhouidi001@gmail.com>
2020-10-19 18:31:32 +0100

    first project

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-$(body).css('color', 'red');
+$(document).ready(function(){
+    $('.Submit').on('click',function(e){
+        e.preventDefault()
+        alert("we received your message")
+    })
+})
+
+function validate(){
+    var email = $("#username").val();
+    var password = $("#psw").val();
+    if ( email === "" && password === ""){
+        alert("Enter your inputs please!")
+    
+    }
+     else if ( password.length < 5){
+         alert("Your Password is too short!")
+     }
+    else if ( email === "melek" && password !== "melekmelek"){
+        alert("welcome")
+    }
+}
